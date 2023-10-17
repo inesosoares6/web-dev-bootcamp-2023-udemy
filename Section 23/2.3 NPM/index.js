36ff3836e8a19fca519b807344618118a6def677
Inês Soares <inesosoares.6@gmail.com>
2023-10-17 23:34:22 +0100

    clean repo

--- Section 23/2.3 NPM/index.js
+++
@@ -1,7 +0,0 @@
-import generateName from "sillyname";
-import superheroes from "superheroes";
-
-var sillyName = generateName();
-console.log(`My name is ${sillyName}!`);
-
-console.log(`My superhero name is ${superheroes.random()}!`);
